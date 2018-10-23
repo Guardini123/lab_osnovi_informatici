b26f1ca54a99965d46d5f7bd5617869272632166
DESKTOP-PIV6EBB\mi2 <vova.betelev@yandex.ru>
2018-10-23 23:23:49 +0300

    no message

---
+++ task1/main.c
@@ -0,0 +1,16 @@
+
+/**
+ * main.c -- программа "Hello, students!"
+ *
+ * Copyright (c) 2014, Student VOVA <betelev@cs.karelia.ru>
+ *
+ * This code is licensed under a MIT-style license.
+ */
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello, students!\n");
+    return 0;
+}
